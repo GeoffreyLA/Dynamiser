11f544fe22f5605ad9f4b7ebc7856739bc157774
GeoffreyLafon <geoffreylafon71@gmail.com>
2023-09-22 10:52:43 +0200

    add script

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ const form = document.querySelector("form");
 const error = document.getElementById("error-message");
 
 form.addEventListener("submit", (event) => {
-  if (firstName.value == "" || lastName.value === "" || message.value === "") {
+  if (firstName.value === "" || lastName.value === "" || message.value === "") {
     showError();
+    deleteFields();
+  } else {
+    deleteFields();
   }
   event.preventDefault();
 });
@@ -35,16 +38,19 @@ function addCommentValue() {
   );
   const newCommentContent = document.createTextNode(message.value);
 
-  newDiv.classList.add("flex" + "space-x-4" + "text-sm" + "text-gray-500");
-  newH3.classList.add("font-medium" + "text-gray-900");
+  newDiv.classList.add("text-sm");
+  newH3.classList.add("font-medium", "text-gray-900");
   newP.classList.add(
-    "prose" + "prose-sm" + "mt-4" + "max-w-none" + "text-gray-500"
+    "prose",
+    "prose-sm",
+    "mt-4",
+    "max-w-none",
+    "text-gray-500"
   );
 
   newDiv.appendChild(newH3);
   newDiv.appendChild(newP);
   newH3.appendChild(newTextH3);
-  newDiv.appendChild(newCommentContent);
   newP.appendChild(newCommentContent);
 
   commentList.appendChild(newDiv);
